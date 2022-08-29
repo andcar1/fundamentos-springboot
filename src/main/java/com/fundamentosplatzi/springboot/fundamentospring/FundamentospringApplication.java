90740bca555b3964636c01c613f6866777747d32
Andrea Carolina Pineda Ovalle <andrea.pineda@and.gov.co>
2022-08-28 22:14:36 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentospring/FundamentospringApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamentospring/FundamentospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
